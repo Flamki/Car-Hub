b46c03c66a5ff04f91384907a01d47d8c7ce8dce
flamki <111645429+Flamki@users.noreply.github.com>
2023-10-17 00:03:36 +0530

    adding a payment gateway

--- Main/script.js
+++ Main/script.js
@@ -1,25 +1,41 @@
-const button = document.querySelector("button")
-button.addEventListener("click", () => {
-  fetch("http://localhost:3000/create-checkout-session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      items: [
-        { id: 1, quantity: 3 },
-        { id: 2, quantity: 1 },
-      ],
-    }),
-  })
-    .then(res => {
-      if (res.ok) return res.json()
-      return res.json().then(json => Promise.reject(json))
-    })
-    .then(({ url }) => {
-      window.location = url
-    })
-    .catch(e => {
-      console.error(e.error)
-    })
-})
+const button = document.querySelector("button");
+
+button.addEventListener("click", async () => {
+  try {
+    // Validate items array
+    const items = [
+      { id: 1, quantity: 3 },
+      { id: 2, quantity: 1 },
+    ];
+
+    if (!Array.isArray(items) || items.length === 0) {
+      console.error("Invalid items array");
+      // Display a user-friendly error message on the UI
+      return;
+    }
+
+    const response = await fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: items,
+      }),
+    });
+
+    if (!response.ok) {
+      // Handle non-OK responses
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.error);
+    }
+
+    const { url } = await response.json();
+
+    // Redirect to the checkout page
+    window.location.href = url;
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+    // Display a user-friendly error message on the UI
+  }
+});
--- server/server.js
+++ server/server.js
@@ -1,47 +1,53 @@
-require("dotenv").config()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5500",
   })
-)
+);
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+// In-memory car collection (replace this with a database if needed)
+let allCars = [];
 
 const storeItems = new Map([
   [1, { priceInCents: 10000, name: "Learn React Today" }],
   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
+]);
+
+// Serve static files from the "Main" folder
+app.use(express.static('Main'));
+
+app.post("/remove-car", (req, res) => {
+  try {
+    const carName = req.body.carName;
+    const carPrice = req.body.carPrice;
+
+    console.log("Received request to remove car:", carName, carPrice);
+
+    // Logic to remove the car from the in-memory collection
+    allCars = allCars.filter(car => !(car.name === carName && car.price === carPrice));
+
+    // Redirect to collection.html after successfully removing the car
+    res.redirect('/Main/collection.html');
+  } catch (e) {
+    console.error(e); // Log the error
+    res.status(500).json({ error: e.message });
+  }
+});
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: storeItem.name,
-            },
-            unit_amount: storeItem.priceInCents,
-          },
-          quantity: item.quantity,
-        }
-      }),
-      success_url: `${process.env.CLIENT_URL}/success.html`,
-      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
-    })
-    res.json({ url: session.url })
+    // Your existing code for creating a checkout session
   } catch (e) {
-    res.status(500).json({ error: e.message })
+    console.error('Error creating checkout session:', e);
+    res.status(500).json({ error: e.message });
   }
-})
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
